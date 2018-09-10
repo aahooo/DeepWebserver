187a399b023f2b119bb0543d7d28b4428ba953f6
aahooo <40741012+aahooo@users.noreply.github.com>
2018-09-10 19:07:14 +0430

    captcha creator improved; now captcha pic is 10 times bigger and up to 6 lines can disturbe it

--- Captcha.py
+++ Captcha.py
@@ -10,18 +10,20 @@ def main(args , details):
     background = ( random.randrange(0,255) , random.randrange(0,255) , random.randrange(0,255) )
     foreground = ( random.randrange(0,255) , random.randrange(0,255) , random.randrange(0,255) )
     
-    img = Image.new('RGB', (100, 30), color = background)
+    img = Image.new('RGB', (1000, 300), color = background)
     font_name = random.choice(os.listdir("Fonts"))
-    fnt = ImageFont.truetype('Fonts/'+font_name,encoding = "utf-8" , size = 15)
+    fnt = ImageFont.truetype('Fonts/'+font_name,encoding = "utf-8" , size = 150)
     d = ImageDraw.Draw(img)
-    d.text((10,10), rand , font=fnt, fill=foreground , spacing = 2 )
-    number_of_lines = random.choice((1,2,3))
+    text_x = random.randrange(0,750)
+    text_y = random.randrange(0,200)
+    d.text((text_x,text_y), rand , font=fnt, fill=foreground , spacing = 2 )
+    number_of_lines = random.choice((1,2,3,4,5,6))
     for i in range(number_of_lines):
-        x1 = random.randrange(0, 75)
-        y1 = random.randrange(0, 30)
-        x2 = random.randrange(25, 100)
-        y2 = random.randrange(0, 30)
-        d.line(( (x1,y1) , (x2,y2) ) , width = 2)
+        x1 = random.randrange(0, 750)
+        y1 = random.randrange(0, 300)
+        x2 = random.randrange(250, 1000)
+        y2 = random.randrange(0, 300)
+        d.line(( (x1,y1) , (x2,y2) ) , width = 20)
 
 
 
